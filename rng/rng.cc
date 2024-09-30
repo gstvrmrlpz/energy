44b7d222eda2baf6acefbf3a3cc0904598994c67
gstvrmrlpz <gustavo@ugr.es>
2024-09-30 20:42:17 +0200

    remove default_random_engine because it is repeated

--- rng/rng.cc
+++ rng/rng.cc
@@ -10,20 +10,20 @@
 using namespace std::chrono_literals;
 using namespace std::literals;
 
-template<typename F> std::size_t test(F f = F())
+template<typename Engine> std::size_t test(Engine engine = Engine())
 {
     const std::size_t N = 1'000'000;
     std::size_t r = 0;
 
     for (std::size_t j = 0; j < N; ++j)
-        r ^= f();
+        r ^= engine();
 
     return r;
 }
 
 std::string_view parser(int argc, char **argv)
 {
-    std::string_view engine = "default_random_engine";
+    std::string_view engine = "minstd_rand0";
     int option = 0;
 
     while ((option = getopt(argc, argv, "e:h")) != -1)
@@ -34,22 +34,21 @@ std::string_view parser(int argc, char **argv)
                 break;
             case 'h':
                 std::cout << argv[0] << " options:\n"
-                          << "\t-e: random engine          (optional)\n"
-                          << "\t    default_random_engine   (default)\n"
+                          << "\t-e: random engine             (optional)\n"
                           << "\t    knuth_b\n"
-                          << "\t    minstd_rand0    (same as default)\n"
+                          << "\t    minstd_rand0 (default_random_engine)\n"
                           << "\t    minstd_rand\n"
                           << "\t    mt19937\n"
                           << "\t    mt19937_64\n"
                           << "\t    mt11213b\n"
-                          << "\t    rand_t\n"
+                          << "\t    rand\n"
                           << "\t    ranlux24_base\n"
                           << "\t    ranlux48_base\n"
                           << "\t    ranlux24\n"
                           << "\t    ranlux48\n"
-                          << "\t    xoroshiro128p\n"
-                          << "\t    xoshiro256p\n"
-                          << "\t-h: this help             (optional)\n";
+                          << "\t    xoroshiro128+\n"
+                          << "\t    xoshiro256+\n"
+                          << "\t-h: this help                (optional)\n";
                 exit(EXIT_SUCCESS);
                 break;
         }
@@ -61,9 +60,7 @@ int main(int argc, char *argv[])
 {
     auto engine = parser(argc, argv);
 
-    if (engine == "default_random_engine")
-        return test<std::default_random_engine>();
-    else if (engine == "knuth_b")
+    if (engine == "knuth_b")
         return test<std::knuth_b>();
     else if (engine == "minstd_rand0")
         return test<std::minstd_rand0>();
@@ -75,7 +72,7 @@ int main(int argc, char *argv[])
         return test<std::mt19937_64>();
     else if (engine == "mt11213b")
         return test<mt11213b>();
-    else if (engine == "rand_t")
+    else if (engine == "rand")
         return test<rand_t>();
     else if (engine == "ranlux24_base")
         return test<std::ranlux24_base>();
@@ -85,9 +82,9 @@ int main(int argc, char *argv[])
         return test<std::ranlux24>();
     else if (engine == "ranlux48")
         return test<std::ranlux48>();
-    else if (engine == "xoroshiro128p")
+    else if (engine == "xoroshiro128+")
         return test<xoroshiro128p>();
-    else if (engine == "xoshiro256p")
+    else if (engine == "xoshiro256+")
         return test<xoshiro256p>();
     else
     {
